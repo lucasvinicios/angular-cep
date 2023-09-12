ea3c848cf46f22a1478f76fbd1f4ea20926593ca
Lucas Dias <lucaswinyb@gmail.com>
2023-09-12 16:18:10 -0300

    indicação se encontrou o CEP ou não

--- src/app/componentes/cep/cep.component.ts
+++ src/app/componentes/cep/cep.component.ts
@@ -16,6 +16,7 @@ export class CepComponent implements OnInit {
   cep = '';
   cepResultado: any = '';
   mensagemErro: string | null = '';
+  cepEncontrado: string | null = '';
 
   constructor(private service: BuscacepService) { }
 
